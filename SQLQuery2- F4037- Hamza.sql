7802213f69bec35d24257fc8d59cf870f2e900b4
Hamza ÖZTÜRK <115626281+hamza2418@users.noreply.github.com>
2023-01-23 01:34:14 +0300

    Add files via upload

---
+++ SQLQuery2- F4037- Hamza.sql
@@ -0,0 +1,53 @@
+--first question
+select c.customer_id, c.first_name, c.last_name, pro.product_name
+from product.product pro
+join sale.order_item oi
+on pro.product_id=oi.product_id
+join sale.orders o 
+on oi.order_id=o.order_id
+join sale.customer c 
+on c.customer_id=o.customer_id
+where pro.product_name='Polk Audio - 50 W Woofer - Black'
+and c.customer_id IN(
+		select c.customer_id
+		from product.product pro
+		join sale.order_item oi
+		on pro.product_id=oi.product_id
+		join sale.orders o 
+		on oi.order_id=o.order_id
+		join sale.customer c 
+		on c.customer_id=o.customer_id
+		where pro.product_name =  '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD')
+
+
+select c.customer_id, c.first_name, c.last_name, pro.product_name
+  --case pro.product_name
+	 -- when 'Polk Audio - 50 W Woofer - Black' then 'YES'
+	 -- else 'NO'
+ -- end as other_product
+from product.product pro
+join sale.order_item oi
+on pro.product_id=oi.product_id
+join sale.orders o 
+on oi.order_id=o.order_id
+join sale.customer c 
+on c.customer_id=o.customer_id
+where c.customer_id IN(
+		select c.customer_id
+		from product.product pro
+		join sale.order_item oi
+		on pro.product_id=oi.product_id
+		join sale.orders o 
+		on oi.order_id=o.order_id
+		join sale.customer c 
+		on c.customer_id=o.customer_id
+		where pro.product_name =  '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD')
+
+
+
+
+
+--second question
+CREATE TABLE ECommerce (	Visitor_ID INT IDENTITY (1, 1) PRIMARY KEY,	Adv_Type VARCHAR (255) 
+NOT NULL,	Action1 VARCHAR (255) NOT NULL);
+INSERT INTO ECommerce (Adv_Type, Action1)VALUES ('A', 'Left'),('A', 'Order'),('B', 'Left'),('A', 'Order'),('A', 'Review'),('A', 'Left'),('B', 'Left'),('B', 'Order'),('B', 'Review'),('A', 'Review');
